56d544ae424cde14af1a9a0d3f473af7b842105e
Mochizuki Takamasa <t-mochizuki@users.noreply.github.com>
2015-11-23 20:57:04 +0900

    fix comments

--- src/main/scala/scala/util/parsing/bnf/BNF.scala
+++ src/main/scala/scala/util/parsing/bnf/BNF.scala
@@ -6,15 +6,14 @@ import scala.util.parsing.combinator._
 object BNF extends RegexParsers {
 
   override val skipWhitespace = false
-  // override val whiteSpace = "".r
 
   /**
-   * syntax     ::=  { rule }
-   * rule       ::=  identifier  "::="  expression
-   * expression ::=  term { "|" term }
-   * term       ::=  factor { factor }
-   * factor     ::=  identifier | quoted_symbol | "("  expression  ")" | "["  expression  "]" | "{"  expression  "}"
-   * identifier ::=  letter { letter | digit }
+   * syntax        ::= { rule }
+   * rule          ::= identifier "::=" expression
+   * expression    ::= term { "|" term }
+   * term          ::= factor { factor }
+   * factor        ::= identifier | quoted_symbol | "("  expression  ")" | "["  expression  "]" | "{"  expression  "}"
+   * identifier    ::= letter { letter | digit }
    * quoted_symbol ::= """ { any_character } """
    */
 
