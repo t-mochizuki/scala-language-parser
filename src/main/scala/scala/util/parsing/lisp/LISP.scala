832bcc8e0b700c9cfefae941eab0919ea2384a82
Mochizuki Takamasa <t-mochizuki@users.noreply.github.com>
2015-11-23 21:26:26 +0900

    fix comments

--- src/main/scala/scala/util/parsing/lisp/LISP.scala
+++ src/main/scala/scala/util/parsing/lisp/LISP.scala
@@ -8,13 +8,13 @@ object LISP extends RegexParsers {
   override val skipWhitespace = false
 
   /**
-   * s_expression ::= atomic_symbol | "(" s_expression "." s_expression ")" | list
-   * list ::= "(" s_expression {s_expression} ")"
+   * s_expression  ::= atomic_symbol | "(" s_expression "." s_expression ")" | list
+   * list          ::= "(" s_expression {s_expression} ")"
    * atomic_symbol ::= letter atom_part
-   * atom_part ::= empty | letter atom_part | number atom_part
-   * letter ::= "a" | "b" | "c" | "d" | "e" | "f" | "g" | "h" | "i" | "j" | "k" | "l" | "m" | "n" | "o" | "p" | "q" | "r" | "s" | "t" | "u" | "v" | "w" | "x" | "y" | "z"
-   * number ::= "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9" | "0"
-   * empty ::= " "
+   * atom_part     ::= empty | letter atom_part | number atom_part
+   * letter        ::= "a" | "b" | "c" | "d" | "e" | "f" | "g" | "h" | "i" | "j" | "k" | "l" | "m" | "n" | "o" | "p" | "q" | "r" | "s" | "t" | "u" | "v" | "w" | "x" | "y" | "z"
+   * number        ::= "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9" | "0"
+   * empty         ::= " "
    */
 
   def s_expression: Parser[Any] = (atomic_symbol | "(" ~ s_expression ~ rep(empty) ~ "." ~ rep(empty) ~ s_expression ~ ")" | list)
