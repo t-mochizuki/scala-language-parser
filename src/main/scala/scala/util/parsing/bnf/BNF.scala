5da932cb14de7ce9f8a07e401f7d09c4255873d8
Mochizuki Takamasa <t-mochizuki@users.noreply.github.com>
2015-11-23 14:49:51 +0900

    decrease Regular Expression

--- src/main/scala/scala/util/parsing/bnf/BNF.scala
+++ src/main/scala/scala/util/parsing/bnf/BNF.scala
@@ -26,8 +26,10 @@ object BNF extends RegexParsers {
   def identifier = rep(space) ~> letter ~ (letter | digit).* <~ rep(space)
   def quoted_symbol = rep(space) ~> "\"" ~> any_character <~ "\"" <~ rep(space)
   def any_character = ".".r
-  def letter = "[a-zA-Z]".r
-  def digit = "[0-9]".r
-  def space = " ".r
+  def letter = lower | upper
+  def lower = "a" | "b" | "c" | "d" | "e" | "f" | "g" | "h" | "i" | "j" | "k" | "l" | "m" | "n" | "o" | "p" | "q" | "r" | "s" | "t" | "u" | "v" | "w" | "x" | "y" | "z"
+  def upper = "A" | "B" | "C" | "D" | "E" | "F" | "G" | "H" | "I" | "J" | "K" | "L" | "M" | "N" | "O" | "P" | "Q" | "R" | "S" | "T" | "U" | "V" | "W" | "X" | "Y" | "Z"
+  def digit = "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9" | "0"
+  def space = " "
   def parse(input: String) = parseAll(syntax, input)
 }
