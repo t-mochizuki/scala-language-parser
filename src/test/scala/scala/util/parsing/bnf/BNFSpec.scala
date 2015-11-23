942d94499da1cce4cf08803d603e48d06f7bd48e
Mochizuki Takamasa <t-mochizuki@users.noreply.github.com>
2015-11-23 14:20:34 +0900

    use FunSpec and MustMatchers

--- src/test/scala/scala/util/parsing/bnf/BNFSpec.scala
+++ src/test/scala/scala/util/parsing/bnf/BNFSpec.scala
@@ -1,19 +1,20 @@
 package scala.util.parsing.bnf
 
-import org.scalatest.FlatSpec
+import org.scalatest.FunSpec
+import org.scalatest.matchers.MustMatchers
 
-class BNFSpec extends FlatSpec {
+class BNFSpec extends FunSpec with MustMatchers {
 
-  behavior of "parse"
-
-  it should "parse BNF rules of LISP" in {
-    BNF.parse("empty ::= \" \"")
-    BNF.parse("""empty ::= " """")
-    BNF.parse("""number ::= "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9" | "0"""")
-    BNF.parse("""letter ::= "a" | "b" | "c" | "d" | "e" | "f" | "g" | "h" | "i" | "j" | "k" | "l" | "m" | "n" | "o" | "p" | "q" | "r" | "s" | "t" | "u" | "v" | "w" | "x" | "y" | "z"""")
-    BNF.parse("atom ::= empty | letter atom | number atom")
-    BNF.parse("atomic ::= letter atom")
-    BNF.parse("""list ::= "(" expr {expr} ")"""")
-    BNF.parse("""expr ::= atomic | "(" expr "." expr ")" | list""")
+  describe("parse") {
+    it("parse BNF rules of LISP") {
+      BNF.parse("empty ::= \" \"") must not be ""
+      BNF.parse("""empty ::= " """") must not be ""
+      BNF.parse("""number ::= "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9" | "0"""") must not be ""
+      BNF.parse("""letter ::= "a" | "b" | "c" | "d" | "e" | "f" | "g" | "h" | "i" | "j" | "k" | "l" | "m" | "n" | "o" | "p" | "q" | "r" | "s" | "t" | "u" | "v" | "w" | "x" | "y" | "z"""") must not be ""
+      BNF.parse("atom ::= empty | letter atom | number atom") must not be ""
+      BNF.parse("atomic ::= letter atom") must not be ""
+      BNF.parse("""list ::= "(" expr {expr} ")"""") must not be ""
+      BNF.parse("""expr ::= atomic | "(" expr "." expr ")" | list""") must not be ""
+    }
   }
 }
